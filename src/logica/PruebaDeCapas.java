05faf2c4a4ef573f4d04eb1d330a9a15006a39e9
Anahi <anahiandrea.p@gmail.com>
2022-02-19 14:03:59 -0300

    Trabajo terminado

---
+++ src/logica/PruebaDeCapas.java
@@ -0,0 +1,20 @@
+
+package logica;
+
+import igu.Pantalla;
+
+
+public class PruebaDeCapas {
+
+    
+    public static void main(String[] args) {
+       Pantalla panta = new Pantalla();
+       panta.setVisible(true);// a la pantalla que acabo de crear con true la hace visible.
+       panta.setLocationRelativeTo(null);//no toma nada como referencia, lo coloca en el medio
+       
+    }
+    
+    
+    
+    
+}
